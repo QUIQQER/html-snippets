066085f9c5835e9386f061ab30ae5456b694fda6
Henning Leutz <leutz@pcsg.de>
2023-12-11 08:09:26 +0100

    feat: html snippet control

--- bin/backend/controls/SnippetInput.js
+++ bin/backend/controls/SnippetInput.js
@@ -21,6 +21,7 @@ define('package/quiqqer/html-snippets/bin/backend/controls/SnippetInput', [
 
         Binds: [
             '$onImport',
+            '$onChange',
             '$load'
         ],
 
@@ -53,50 +54,78 @@ define('package/quiqqer/html-snippets/bin/backend/controls/SnippetInput', [
 
             this.Loader.inject(this.$Elm);
             this.Loader.show();
-            this.$load();
+            this.$load().then(() => {
+                this.$Textarea.addEvents({
+                    change: this.$onChange,
+                    blur: this.$onChange
+                });
+            });
         },
 
+        $onChange: function() {
+            this.Loader.show();
 
-        save: function() {
-            console.log(111);
-        },
-
-        $load: function() {
-            if (this.$loaded) {
-                return;
-            }
-
-            if (!this.$Project) {
-                return;
-            }
-
-            this.$loaded = true;
-
-            QUIAjax.get('package_quiqqer_html-snippets_ajax_backend_getSnippet', (snippetData) => {
-                this.$Textarea.value = snippetData.snippet;
+            QUIAjax.post('package_quiqqer_html-snippets_ajax_backend_saveSnippet', () => {
                 this.Loader.hide();
             }, {
                 'package': 'quiqqer/html-snippets',
                 projectName: this.$Project.getName(),
                 snippetName: this.getAttribute('snippetName'),
-                showError: false,
-                onError: (err) => {
-                    // create snippet if snippet not exists
-                    console.error(err);
+                eventName: this.getAttribute('snippetEvent'),
+                snippet: this.$Textarea.value
+            });
+        },
 
-                    if (err.getCode() === 404) {
-                        this.$loaded = false;
+        $load: function() {
+            if (this.$loaded) {
+                return Promise.resolve();
+            }
 
-                        QUIAjax.post('package_quiqqer_html-snippets_ajax_backend_addSnippet', () => {
-                            this.$load();
-                        }, {
-                            'package': 'quiqqer/html-snippets',
-                            projectName: this.$Project.getName(),
-                            snippetName: this.getAttribute('snippetName'),
-                            eventName: this.getAttribute('snippetEvent')
-                        });
+            if (this.$Textarea.value !== '') {
+                this.$loaded = true;
+                this.Loader.hide();
+
+                return Promise.resolve();
+            }
+
+            if (!this.$Project) {
+                return Promise.resolve();
+            }
+
+            this.$loaded = true;
+
+            return new Promise((resolve) => {
+                QUIAjax.get('package_quiqqer_html-snippets_ajax_backend_getSnippet', (snippetData) => {
+                    this.$Textarea.value = snippetData.snippet;
+                    this.Loader.hide();
+                    resolve();
+                }, {
+                    'package': 'quiqqer/html-snippets',
+                    projectName: this.$Project.getName(),
+                    snippetName: this.getAttribute('snippetName'),
+                    showError: false,
+                    onError: (err) => {
+                        // create snippet if snippet not exists
+                        console.error(err);
+
+                        if (err.getCode() === 404) {
+                            this.$loaded = false;
+
+                            QUIAjax.post('package_quiqqer_html-snippets_ajax_backend_addSnippet', () => {
+                                this.$load().then(resolve);
+                            }, {
+                                'package': 'quiqqer/html-snippets',
+                                projectName: this.$Project.getName(),
+                                snippetName: this.getAttribute('snippetName'),
+                                eventName: this.getAttribute('snippetEvent')
+                            });
+
+                            return;
+                        }
+
+                        resolve();
                     }
-                }
+                });
             });
         }
     });
