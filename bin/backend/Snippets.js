0eba3fe3a0602872d9b87d07e1600017f98bc86d
Henning Leutz <leutz@pcsg.de>
2024-06-18 10:46:13 +0200

    fix: label click/hover events prevented

--- bin/backend/Snippets.js
+++ bin/backend/Snippets.js
@@ -60,13 +60,18 @@ define('package/quiqqer/html-snippets/bin/backend/Snippets', [
 
         $onImport: function() {
             const Label = this.getElm().getParent('label');
+            const Parent = Label.parentNode;
             Label.getElements('.field-container-item').destroy();
 
             const Container = new Element('div', {
                 styles: {
                     width: '100%'
                 }
-            }).inject(Label);
+            });
+
+            Parent.appendChild(this.$Elm);
+            Parent.replaceChild(Container, Label);
+
 
             SnippetUtils.isGDPRInstalled().then((isInstalled) => {
 
@@ -99,13 +104,14 @@ define('package/quiqqer/html-snippets/bin/backend/Snippets', [
                 }
 
                 this.$Grid = new Grid(Container, {
-                    height: Label.getParent('.qui-panel-content').getSize().y - 100,
+                    height: Parent.getParent('.qui-panel-content').getSize().y - 100,
                     multipleSelection: true,
                     columnModel: columnModel,
                     buttons: [
                         {
-                            name: 'add',
+                            name: 'addSnippet',
                             text: QUILocale.get('quiqqer/core', 'add'),
+                            position: 'left',
                             events: {
                                 click: this.add
                             }
@@ -178,7 +184,7 @@ define('package/quiqqer/html-snippets/bin/backend/Snippets', [
             if (!this.$Project) {
                 return;
             }
-
+console.log(123);
             require([
                 'package/quiqqer/html-snippets/bin/backend/controls/windows/AddSnippet'
             ], (AddSnippet) => {
